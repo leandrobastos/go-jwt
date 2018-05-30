d8794290f652ef6df711e1a1def5081205e70617
Leandro Bastos <leandrobastos@MacBook-Pro-de-Leandro.local>
2018-05-30 11:20:06 -0300

    Settings

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	"github.com/mitchellh/mapstructure"
+	uuid "github.com/satori/go.uuid"
 )
 
 type User struct {
+	UUID     string `json:"public_id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -26,59 +29,46 @@ type Exception struct {
 	Message string `json:"message"`
 }
 
-func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
+func CreateTokenAuth(w http.ResponseWriter, r *http.Request) {
 	var user User
-	_ = json.NewDecoder(req.Body).Decode(&user)
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"exp":      time.Now().Add(time.Hour * time.Duration(1)).Unix(),
+		"uuid":     uuid.Must(uuid.NewV4()),
 		"username": user.Username,
 		"password": user.Password,
 	})
-	tokenString, error := token.SignedString([]byte("secret"))
+	tokenString, error := token.SignedString([]byte("e0037393-0596-4e41-9f37-459fd262623c-00d219d5-456f-4158-8635-35f36522f299"))
 	if error != nil {
 		fmt.Println(error)
 	}
 	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
 }
 
-func ProtectedEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := req.URL.Query()
-	token, _ := jwt.Parse(params["token"][0], func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("There was an error")
-		}
-		return []byte("secret"), nil
-	})
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		var user User
-		mapstructure.Decode(claims, &user)
-		json.NewEncoder(w).Encode(user)
-	} else {
-		json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
-	}
-}
-
-func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		authorizationHeader := req.Header.Get("authorization")
+func RequireTokenAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorizationHeader := r.Header.Get("authorization")
 		if authorizationHeader != "" {
-			bearerToken := strings.Split(authorizationHeader, " ")
+			bearerToken := strings.Split(authorizationHeader, "Bearer ")
 			if len(bearerToken) == 2 {
-				token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+				token, _ := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
 					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 						return nil, fmt.Errorf("There was an error")
 					}
-					return []byte("secret"), nil
+					return []byte("e0037393-0596-4e41-9f37-459fd262623c-00d219d5-456f-4158-8635-35f36522f299"), nil
 				})
-				if error != nil {
-					json.NewEncoder(w).Encode(Exception{Message: error.Error()})
-					return
-				}
+				// if error != nil {
+				// 	json.NewEncoder(w).Encode(Exception{Message: error.Error()})
+				// 	return
+				// }
 				if token.Valid {
-					context.Set(req, "decoded", token.Claims)
-					next(w, req)
+					context.Set(r, "decoded", token.Claims)
+					next(w, r)
 				} else {
 					json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
 				}
+			} else {
+				json.NewEncoder(w).Encode(Exception{Message: "Invalid token"})
 			}
 		} else {
 			json.NewEncoder(w).Encode(Exception{Message: "An authorization header is required"})
@@ -86,8 +76,8 @@ func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	})
 }
 
-func TestEndpoint(w http.ResponseWriter, req *http.Request) {
-	decoded := context.Get(req, "decoded")
+func UserData(w http.ResponseWriter, r *http.Request) {
+	decoded := context.Get(r, "decoded")
 	var user User
 	mapstructure.Decode(decoded.(jwt.MapClaims), &user)
 	json.NewEncoder(w).Encode(user)
@@ -96,8 +86,7 @@ func TestEndpoint(w http.ResponseWriter, req *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	fmt.Println("Starting the application...")
-	router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
-	router.HandleFunc("/protected", ProtectedEndpoint).Methods("GET")
-	router.HandleFunc("/test", ValidateMiddleware(TestEndpoint)).Methods("GET")
+	router.HandleFunc("/token-auth", CreateTokenAuth).Methods("POST")
+	router.HandleFunc("/user", RequireTokenAuth(UserData)).Methods("GET")
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
